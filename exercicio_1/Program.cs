0673a9eb65ade38ac524187d85124cee9ad4b95f
Luiz Eduardo <163674230+LuizBiscola@users.noreply.github.com>
2025-01-28 17:55:56 -0300

    Exercicio calculadora

--- exercicio_1/Program.cs
+++ exercicio_1/Program.cs
@@ -1,19 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Luiz Eduardoi");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Luiz Eduardoi");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Luiz Eduardoi");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Luiz Eduardoi");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Luiz Eduardoi");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Luiz Eduardoi");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Luiz Eduardoi");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Luiz Eduardoi");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Luiz Eduardoi");
+﻿class Program
+{
+    static void Main()
+    {
+        float resultado, numero, numero1;
+        char operacao, opcao;
+        
+        resultado = 0;
+        do
+        {
+            Console.WriteLine("Digite o primeiro numero: ");
+            numero = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero: ");
+            numero1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a aoperção (+, -, /, *)");
+            operacao = char.Parse(Console.ReadLine());
+
+            switch (operacao)
+            {
+                case '+':
+                    resultado = numero + numero1;
+                    break;
+                case '-':
+                    resultado = numero - numero1;
+                    break;
+                case '*': 
+                    resultado = numero * numero1;
+                    break;
+                case '/':
+                    resultado = numero / numero1;
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine($"O resultado é {resultado}");
+                
+            Console.WriteLine("Deseja realizar uma nova operação?(S/N)");
+            opcao = char.Parse(Console.ReadLine());   
+
+        } while (opcao == 'S' || opcao == 's');
+    }
+}
