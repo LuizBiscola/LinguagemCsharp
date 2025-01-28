9533220ea5b76453a413a7e013d7e7cd5b0925de
Luiz Eduardo <163674230+LuizBiscola@users.noreply.github.com>
2025-01-28 17:41:31 -0300

    tyeste 2

--- exercicio_1/Program.cs
+++ exercicio_1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Luiz Eduardoi");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Luiz Eduardoi");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Luiz Eduardoi");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Luiz Eduardoi");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Luiz Eduardoi");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Luiz Eduardoi");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Luiz Eduardoi");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Luiz Eduardoi");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Luiz Eduardoi");
