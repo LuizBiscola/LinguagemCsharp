2260cccc72d3568f478068402b070f9c0462be9d
Luiz Eduardo <163674230+LuizBiscola@users.noreply.github.com>
2025-01-28 18:29:26 -0300

    .

---
+++ alimentar_vetor/Program.cs
@@ -0,0 +1,22 @@
+﻿// alimentar um vetor, somar e mostrar na tela
+class Program
+{
+    static void Main ()
+    {
+        int[] vetor = new int[5];
+        int soma = 0;
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = 0;
+        }
+
+        for (int i = 0; i <  vetor.Length; i++) 
+        {
+            Console.Write($"{vetor[i]}");
+            soma = soma + vetor[i];
+        }
+        Console.WriteLine("");
+        Console.WriteLine($"Soma é {soma}");
+    }
+}
